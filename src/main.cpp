d0ecd584d8a6f662fdbd0d9b5683e5636ef7bfd3
kaezr <107052359+kaezrr@users.noreply.github.com>
2024-09-18 16:55:40 +0530

    Update .gitignore file

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #define SDL_MAIN_HANDLED
+#include <iostream>
 #include <SDL.h>
 #include "gameboy.hpp"
 
-int main(int, char*[]) {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+int main(int argc, char*[] argv) {
+    if(argc < 2 || argc > 3) {
+        std::cout << "Usage: " << argv[0] << "[path/to/rom] " << "[path/to/boot-rom](optional)\n";
+        return 1;
+    }
 
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     GameBoy GB{};
 
-    //GB.load_boot("roms/dmg_boot.bin");
-    GB.load_game("roms/tennis.gb");
+    if(arc == 3) 
+        GB.load_boot(argv[2]);
+    GB.load_game(argv[1]);
 
     GB.start();
     SDL_Quit();
