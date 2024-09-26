b97aca442006128306bd56d0ca8aa6621ec90ed8
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-09-26 09:47:01 +0000

    Replace std::format with fmt::format in match_file and memory_dump

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "gameboy.hpp"
 
 int main(int argc, char** argv) {
-    spdlog::set_pattern("%v");
+    spdlog::set_pattern("%^[%l]%$ %v");
     if(argc < 2 || argc > 3) {
         spdlog::error("Usage: {} [path/to/rom] [path/to/boot-rom](optional)", argv[0]);
         return 1;
@@ -15,14 +15,13 @@ int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     GameBoy GB{};
 
-    if(argc == 3) 
-        GB.load_boot(argv[2]);
-
+    if(argc == 3) GB.load_boot(argv[2]);
     GB.load_game(argv[1]);
 
     spdlog::info("{} Version {}", PROJECT_NAME, PROJECT_VERSION);
     GB.start();
-    SDL_Quit();
 
+    spdlog::shutdown();
+    SDL_Quit();
     return 0;
 }
--- src/utils/debug.cpp
+++ src/utils/debug.cpp
@@ -1,31 +1,26 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 #include "debug.hpp"
 #include "mnemonic.hpp"
 
-void Debugger::log_path(const string& path) {
-    auto logger = spdlog::basic_logger_mt("disassembly-log",  path);
-    
-}
-
 // Print disassembly
 void Debugger::write_text_log() {
-    file << std::format("[{:05d}] [{:04x}] [{:02x} {:02x} {:02x}] ", sm83->elapsed_cycles, sm83->PC.full,
-        mem->read(sm83->PC.full), mem->read(sm83->PC.full + 1), mem->read(sm83->PC.full + 2));
-    file << get_op(mem->read(sm83->PC.full)) << '\n';
+  file << std::format("[{:05d}] [{:04x}] [{:02x} {:02x} {:02x}] ",
+                      sm83->elapsed_cycles, sm83->PC.full,
+                      mem->read(sm83->PC.full), mem->read(sm83->PC.full + 1),
+                      mem->read(sm83->PC.full + 2));
+  file << get_op(mem->read(sm83->PC.full)) << '\n';
 
-    if (sm83->PC.full == 0xCB) {
-    file << std::format("[{:05d}] [{:04x}] [{:02x} {:02x} {:02x}] ", sm83->elapsed_cycles, sm83->PC.full + 1,
-        mem->read(sm83->PC.full+1), mem->read(sm83->PC.full + 2), mem->read(sm83->PC.full + 3));
+  if (sm83->PC.full == 0xCB) {
+    file << std::format(
+        "[{:05d}] [{:04x}] [{:02x} {:02x} {:02x}] ", sm83->elapsed_cycles,
+        sm83->PC.full + 1, mem->read(sm83->PC.full + 1),
+        mem->read(sm83->PC.full + 2), mem->read(sm83->PC.full + 3));
     file << get_cb(mem->read(sm83->PC.full + 1)) << '\n';
     }
 
-    print_cpu_mem(file);
-}
-
-// Print cpu state, important registers
-void Debugger::print_cpu_mem(std::ostream& out) {
     out << std::format( "[AF: ${:04x} BC: ${:04x} DE: ${:04x} ",
         sm83->AF.full, sm83->BC.full, sm83->DE.full);
     out << std::format("HL: ${:04x} SP: ${:04x} PC: ${:04x}\nLY: ${:02x} STAT: ${:02x} ",
@@ -34,25 +29,28 @@ void Debugger::print_cpu_mem(std::ostream& out) {
 }
 
 void Debugger::write_match_log() {
-    string str = std::format("a:{:02x} ", sm83->AF.hi) + "f:";
+    auto match_logger = spdlog::basic_logger_mt("match-log", "logs/match.txt");
+    string str = fmt::format("a:{:02x} ", sm83->AF.hi) + "f:";
     str += (sm83->get_flag(Z) ? 'z' : '-');
     str += (sm83->get_flag(N) ? 'n' : '-');
     str += (sm83->get_flag(H) ? 'h' : '-');
     str += (sm83->get_flag(C) ? 'c' : '-');
 
-    str += std::format(" bc:{:04x} de:{:04x} hl:{:04x} sp:{:04x} pc:{:04x} (cy: {:d})\n",
+    str += fmt::format(" bc:{:04x} de:{:04x} hl:{:04x} sp:{:04x} pc:{:04x} (cy: {:d})\n",
         sm83->BC.full, sm83->DE.full, sm83->HL.full, sm83->SP.full, sm83->PC.full, sm83->debug_cycles);
-    file << str;
+    match_logger->debug(str);
 }
 
 // Dump memory to a text file
 void Debugger::memory_dump(u16 start, u16 end) {
+    auto memory_dump = spdlog::basic_logger_mt("memory-dump", "logs/dump.txt");
+    string str = "";
     for (u16 i = start; i <= end; i += 0x10) {
-        file << std::format("{:04x}:", i);
+        str += fmt::format("{:04x}:", i);
         for (u16 j = i; j < i + 16; j++) {
-            file << std::format(" {:02x}", mem->read(j));
+            str += fmt::format(" {:02x}", mem->read(j));
         }
-        file << '\n';
+        memory_dump->debug(str);
     }
 }
 
